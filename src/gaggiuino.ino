afd6de8179ea1033f078051106a5707b47ca3657
Vid Tadel <tadelv@gmail.com>
2024-02-24 11:28:42 +0100

    no really

--- src/gaggiuino.ino
+++ src/gaggiuino.ino
@@ -889,7 +889,7 @@ static unsigned long getTimeSinceInit(void) {
 }
 
 static void fillBoiler(void) {
-  #if (defined LEGO_VALVE_RELAY || defined SINGLE_BOARD) && undefined ASCASO
+  #if (defined LEGO_VALVE_RELAY || defined SINGLE_BOARD) && !defined ASCASO
 
   if (systemState.startupInitFinished) {
     return;
